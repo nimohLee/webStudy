245891c9c49fc951618f392d4d36267844923c5f
nimoh <spakers38@gmail.com>
2022-09-07 15:59:18 +0900

    node filesystem

--- nodejs_tut/04-fs-write/02-fs-write.js
+++ nodejs_tut/04-fs-write/02-fs-write.js
@@ -9,8 +9,26 @@ const path = require('path');
 // new Date() : 년월일
 // getFullYear(), getMonth(), getDay()
 
+/* 폴더 생성 */
 let yy = new Date().getFullYear();
 let mm = new Date().getMonth() +1 ;
+if(mm<10){
+    mm = "0"+mm;
+}
 let dd = new Date().getDate();
+if(dd<10){
+    dd = "0"+dd;
+}
 let yymmdd = `${yy}-${mm}-${dd}`;
 console.log(yymmdd);
+
+fs.mkdir(yymmdd,(err)=>{
+    if(err) throw err;
+    console.log('로그폴더 생성');
+})
+
+/* 파일 생성 : 시간 */
+function UNIX_TIMESTAMP(){
+    return new Date().getTime();
+}
+console.log(UNIX_TIMESTAMP());

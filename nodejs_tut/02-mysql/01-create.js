30d2252d682d529ba77b764348ab9d853e14885f
nimoh <spakers38@gmail.com>
2022-09-21 15:56:54 +0900

    rm node-module

--- nodejs_tut/02-mysql/01-create.js
+++ nodejs_tut/02-mysql/01-create.js
@@ -1,19 +1,34 @@
-const express = require('express');
-const mysql = require('mysql');
+const express = require("express");
+const mysql = require("mysql");
 
 const app = express();
 
-app.set('view engine', 'ejs'); // ejs middleware
+//////////////////////////////////////////////
+/*////////////  MySQL DB Connection /////////*/
+//////////////////////////////////////////////
+const mysql = require("mysql");
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "login",
+    password: "1234",
+    database: "login",
+});
 
-app.get('/',(req, res) => { // 버튼 보여주기위한 URL
+connection.connect();
+
+app.set("view engine", "ejs"); // ejs middleware
+
+app.get("/", (req, res) => {
+    // 버튼 보여주기위한 URL
     res.render("create");
 });
 
-app.get('/createTB',(req, res) =>{ // 클릭했을 떄 실행하는 url
-    res.render("create url success");
+app.get("/createTB", (req, res) => {
+    // 클릭했을 떄 실행하는 url
+    console.log("create url success");
 });
-app.get('/dropTB',(req, res) =>{ 
-    res.render("drop url success");
+app.get("/dropTB", (req, res) => {
+    console.log("drop url success");
 });
 
 app.listen(3000, () => console.log("Server Running..."));

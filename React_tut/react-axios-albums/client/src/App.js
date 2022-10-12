d1609e656a4fa4ca21b16afb540d6a265e9a14da
nimoh <spakers38@gmail.com>
2022-10-12 10:45:35 +0900

    keyframes 사용 && loading 구현

--- React_tut/react-axios-albums/client/src/App.js
+++ React_tut/react-axios-albums/client/src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
-
+import axios from 'axios';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import  Loading  from './components/Loading'
 function App() {
+  
+  const URL = "https://jsonplaceholder.typicode.com/album";
+  const [albums,setAlbums] = useState([]);
+  const [isLoading,setIsLoading] = useState(false);
+
+  //useEffect (화살표함수, []);
+  useEffect(()=>{
+    const fetchData = async function(){
+      const result = await axios.get(URL);
+      setAlbums(result.data.slice(0,20));
+      setIsLoading(false);
+    }
+      setIsLoading(true);
+    fetchData();
+  },[]); // [] 없으면 화면 변화가 일어날 때 마다 자료 불러오기 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      
+        {
+          isLoading == true ? <Loading/> : 
+          albums.map((album,key)=> {
+            return(
+              <div key={key}>
+              <h2>{album.id}</h2>
+              <h2>{album.title}</h2>
+              </div>
+            ); 
+          })
+        }
+      
+    </>
   );
 }
 
